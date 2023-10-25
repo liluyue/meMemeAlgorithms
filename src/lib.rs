be005e8b40e2ea11112f2c82d646192f2820e1ba
liluyue <3078570790@qq.com>
2023-10-25 23:12:41 +0800

    feat(huffMan):

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 #![deny(clippy::all)]
-
 #[macro_use]
 extern crate napi_derive;
 
+use algorithms_fourth::string::huff_man;
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+pub fn huff_man_encode(data: String) -> Vec<u8> {
+  let encoder = huff_man::HuffmanEncode::new();
+  encoder.compress(&data)
+}
+#[napi]
+pub fn huff_man_decode(data:Vec<u8>) -> String {
+    let mut decoder = huff_man::HuffmanDecode::new(data);
+    decoder.expand()
 }
