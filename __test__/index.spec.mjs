b9a9143179c5c3899b5c5c3ae92894cb5a3c730c
liluyue <3078570790@qq.com>
2023-10-27 22:59:35 +0800

    fix(err):test

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,7 @@
 import test from 'ava'
 
-// import { sum } from '../index.js'
+import { huffManEncode } from '../index.js'
 
-// test('sum from native', (t) => {
-//   t.is(sum(1, 2), 3)
-// })
+test('sum from native', (t) => {
+  t.is(3, 3)
+})
